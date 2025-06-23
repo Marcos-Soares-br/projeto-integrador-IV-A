1608afc731cfd412dbb8d43901842673c60f1e92
Marcos-Soares-br <marcosantoinioss@gmail.com>
2025-06-23 15:06:57 -0300

    Metodo subtracao criado

--- calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
+++ calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -9,8 +9,11 @@ package com.mycompany.calculadora;
  * @author Marcos
  */
 public class Calculadora {
-
-    public static void main(String[] args) {
-        System.out.println("NetBeans >>>>> vsCode!");
+    public int soma(int x, int y) {
+        return x + y;
+    }
+    
+    public int subtracao(int x, int y) {
+        return x - y;
     }
 }
